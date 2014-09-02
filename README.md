c0e75f8018f6552a5a7d91fadf9d3f55a6658c23
Denilson Sá <denilsonsa@gmail.com>
2014-09-02 03:18:40 -0300

    Adding hex_to_decimal_rgb.py script.
    It was helpful while creating the palettes based on Wikipedia.
    
    Also added other links to the README.

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Sources:
 * https://github.com/grompe/Drawception-ANBT/blob/master/drawception-anbt.user.js
 * [List of 8-bit computer hardware palettes](https://en.wikipedia.org/wiki/List_of_8-bit_computer_hardware_palettes)
 * [List of videogame consoles palettes](https://en.wikipedia.org/wiki/List_of_videogame_consoles_palettes)
+* http://segaretro.org/Palette
 
 Other palettes:
 
 * https://github.com/chriskempson/base16-gimp-palette
 * http://www.ve3syb.ca/software/gimp/2.4/pantone.gpl
+* http://nevit.deviantart.com/art/Gimp-Palettes-82563425
 
 [gimp]: http://www.gimp.org/
---
+++ hex_to_decimal_rgb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Replaces colors in #fff or #ffffff notation with three integers: 255 255 255
+# Useful to generate a Gimp palette based on list of colors in hex notation.
+
+import re
+import sys
+
+def hex_to_rgb(value):
+    if len(value) == 3:
+        value = value[0] * 2 + value[1] * 2 + value[2] * 2
+    return ' '.join(str(x) for x in (
+        int(value[0:2], 16),
+        int(value[2:4], 16),
+        int(value[4:6], 16),
+    ))
+
+for line in sys.stdin.readlines():
+    line = re.sub(
+        r'#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})',
+        lambda match: hex_to_rgb(match.group(1)),
+        line)
+    print(line.strip())
