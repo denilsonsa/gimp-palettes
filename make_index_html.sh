f2427405fef7446b0dc306f6410c5b20f9aa963b
Denilson Sá <denilsonsa@gmail.com>
2014-09-23 02:49:45 -0300

    make_index_html.sh will now switch to gh-pages branch automatically.

--- make_index_html.sh
+++ make_index_html.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-./gpl_to_html.py -o index.html palettes/*.gpl
-sed -i 's,\(href="\)\(palettes/[^"]*\.gpl"\),\1https://raw.githubusercontent.com/denilsonsa/gimp-palettes/master/\2,g' index.html
-sed -i 's,\(<body[^>]*>.*\),\1\n<h1 style="text-align:center"><a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n,' index.html
+set -ex
+
+./gpl_to_html.py -o index_new.html palettes/*.gpl
+sed -i 's|\(href="\)\(palettes/[^"]*\.gpl"\)|\1https://raw.githubusercontent.com/denilsonsa/gimp-palettes/master/\2|g' index_new.html
+sed -i 's|\(<body[^>]*>.*\)|\1\n<h1 style="text-align:center">Palettes for GIMP\, Inkscape\, Calligra/Krita... <a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n|' index_new.html
+
+git checkout gh-pages
+mv index_new.html index.html
+
+if ! git diff --exit-code --quiet ; then
+	git add index.html
+	git commit -m 'auto-updating index.html'
+fi
+
+git checkout master
