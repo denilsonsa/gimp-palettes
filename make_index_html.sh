012072db2f3ce1ba01e0a36003bf1190f1e8be51
Denilson Sá <denilsonsa@gmail.com>
2015-07-21 22:07:15 -0300

    Adding MyPaint and Drawpile to the HTML header.

--- make_index_html.sh
+++ make_index_html.sh
@@ -4,7 +4,7 @@ set -ex
 
 ./gpl_to_html.py -o index_new.html palettes/*.gpl
 sed -i 's|\(href="\)\(palettes/[^"]*\.gpl"\)|\1https://raw.githubusercontent.com/denilsonsa/gimp-palettes/master/\2|g' index_new.html
-sed -i 's|\(<body[^>]*>.*\)|\1\n<h1 style="text-align:center">Palettes for GIMP\, Inkscape\, Calligra/Krita... <a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n|' index_new.html
+sed -i 's|\(<body[^>]*>.*\)|\1\n<h1 style="text-align:center">Palettes for GIMP, Inkscape, Calligra/Krita, MyPaint, Drawpile... <a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n|' index_new.html
 
 git checkout gh-pages
 mv index_new.html index.html
