bc17264b882b7040479330161d1ba8044497a591
Denilson Sá Maia <denilsonsa@gmail.com>
2025-04-23 11:15:09 +0200

    make_index_html.sh is now compatible with MacOS/BSD version of sed
    The `-i` parameter is nice, but it behaves slightly differently on different versions of sed.
    
    As a bonus, by doing this refactor we now have everything as a single shell pipeline, instead of
    rewriting the same file multiple times.

--- make_index_html.sh
+++ make_index_html.sh
@@ -9,7 +9,8 @@ fi
 
 set -ex
 
-./gpl_to_html.py -o "${filename}" palettes/*.gpl
-sed -i 's|\(href="\)\(palettes/[^"]*\.gpl"\)|\1https://raw.githubusercontent.com/denilsonsa/gimp-palettes/master/\2|g' "${filename}"
-sed -i 's|\(</head>\)|<script data-goatcounter="https://denilsonsa.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>\n\1|' "${filename}"
-sed -i 's|\(<body[^>]*>.*\)|\1\n<h1 style="text-align:center">Palettes for GIMP, Inkscape, Calligra/Krita, MyPaint, Aseprite, Drawpile... <a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n|' "${filename}"
+./gpl_to_html.py palettes/*.gpl \
+	| sed 's|\(href="\)\(palettes/[^"]*\.gpl"\)|\1https://raw.githubusercontent.com/denilsonsa/gimp-palettes/master/\2|g' \
+	| sed 's|\(</head>\)|<script data-goatcounter="https://denilsonsa.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>\n\1|' \
+	| sed 's|\(<body[^>]*>.*\)|\1\n<h1 style="text-align:center">Palettes for GIMP, Inkscape, Calligra/Krita, MyPaint, Aseprite, Drawpile... <a href="https://github.com/denilsonsa/gimp-palettes" style="text-decoration:none">https://github.com/denilsonsa/gimp-palettes</a></h1>\n|' \
+	> "${filename}"
